cdccf413d2898ada71f7899eb5c540267da02e99
Choe, Cheng-Dae <charlie.choe@tradlinx.com>
2024-05-31 11:09:16 +0900

    bumpup packages

--- client.go
+++ client.go
@@ -13,7 +13,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/whitekid/goxp"
 	"github.com/whitekid/goxp/log"
-	"github.com/whitekid/goxp/request"
+	"github.com/whitekid/goxp/requests"
 )
 
 //go:generate mockery --name Interface
@@ -30,7 +30,7 @@ type Interface interface {
 type clientImpl struct {
 	consumerKey string
 	accessToken string
-	sess        request.Interface // common sessions
+	sess        requests.Interface // common sessions
 }
 
 var _ Interface = (*clientImpl)(nil)
@@ -40,7 +40,7 @@ func New(consumerKey, accessToken string) Interface {
 	return &clientImpl{
 		consumerKey: consumerKey,
 		accessToken: accessToken,
-		sess:        request.NewSession(nil),
+		sess:        requests.NewSession(nil),
 	}
 }
 
@@ -117,12 +117,12 @@ func (a *Article) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-func (c *clientImpl) post(url string) *request.Request {
+func (c *clientImpl) post(url string) *requests.Request {
 	u, _ := gourl.JoinPath("https://getpocket.com/v3", url)
 	return c.sess.Post(u).Header("X-Accept", "application/json")
 }
 
-func (c *clientImpl) sendRequest(ctx context.Context, req *request.Request) (*request.Response, error) {
+func (c *clientImpl) sendRequest(ctx context.Context, req *requests.Request) (*requests.Response, error) {
 	resp, err := req.Do(ctx)
 	if err != nil {
 		return resp, errors.Wrap(err, "request failed")
@@ -149,12 +149,13 @@ func (c *clientImpl) AuthorizedURL(ctx context.Context, redirectURI string) (str
 		return "", "", errors.Wrap(err, "authorized request failed")
 	}
 
-	var response struct {
+	type Response struct {
 		Code string `json:"code"`
 	}
 
 	defer resp.Body.Close()
-	if err := resp.JSON(&response); err != nil {
+	response, err := goxp.ReadJSON[Response](resp.Body)
+	if err != nil {
 		return "", "", errors.Wrap(err, "fail to parse json")
 	}
 
@@ -174,13 +175,14 @@ func (c *clientImpl) NewAccessToken(ctx context.Context, requestToken string) (s
 		return "", "", err
 	}
 
-	var response struct {
+	type Response struct {
 		AccessToken string `json:"access_token"`
 		Username    string `json:"username"`
 	}
 
 	defer resp.Body.Close()
-	if err := resp.JSON(&response); err != nil {
+	response, err := goxp.ReadJSON[Response](resp.Body)
+	if err != nil {
 		return "", "", err
 	}
 
@@ -385,10 +387,9 @@ func (a *addRequestImpl) Do(ctx context.Context) (*AddResponse, error) {
 		return nil, err
 	}
 
-	response := &AddResponse{}
-
 	defer resp.Body.Close()
-	if err := resp.JSON(response); err != nil {
+	response, err := goxp.ReadJSON[AddResponse](resp.Body)
+	if err != nil {
 		return nil, err
 	}
 
@@ -487,9 +488,9 @@ func (r *modifyRequestImpl) Do(ctx context.Context) (*ModifyResponse, error) {
 		return nil, err
 	}
 
-	response := &ModifyResponse{}
 	defer resp.Body.Close()
-	if err := resp.JSON(response); err != nil {
+	response, err := goxp.ReadJSON[ModifyResponse](resp.Body)
+	if err != nil {
 		return nil, err
 	}
 
--- go.mod
+++ go.mod
@@ -1,22 +1,21 @@
 module github.com/whitekid/getpocket
 
-go 1.20
+go 1.23.3
 
 require (
 	github.com/pkg/errors v0.9.1
-	github.com/stretchr/testify v1.8.4
-	github.com/whitekid/goxp v0.0.0-20230803113103-cb3e9964e00a
+	github.com/stretchr/testify v1.9.0
+	github.com/whitekid/goxp v0.0.0-20241108094850-f7313e0febd8
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/whitekid/iter v0.0.0-20230727022917-a28e6cf0ed40 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	go.uber.org/zap v1.25.0 // indirect
-	golang.org/x/exp v0.0.0-20230801115018-d63ba01acd4b // indirect
-	golang.org/x/net v0.13.0 // indirect
-	golang.org/x/sync v0.3.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sync v0.9.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
